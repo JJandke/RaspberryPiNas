168730fe5ee278554c0598ead8aeade030ac2920
Johannes <johannesjandke@gmail.com>
2021-01-29 17:11:07 +0100

    new comments exact python interpreter location needet

--- cooling_pi.py
+++ cooling_pi.py
@@ -1,4 +1,5 @@
 # usr/bin/python3
+# -*- coding: utf-8 -*-
 
 from gpiozero import CPUTemperature
 import pigpio
@@ -7,13 +8,15 @@ import time
 import datetime
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(4, GPIO.OUT)  # raspberry pi fan
+GPIO.setwarnings(False)     # set setwarnings = False if another script uses the GPIO
+GPIO.setup(4, GPIO.OUT)     # raspberry pi fan
+GPIO.setup(17, GPIO.OUT)    # hdd fan
 pi = pigpio.pi()
 
 now = datetime.datetime.now()
 cpu = CPUTemperature()
 
+# test fans
 GPIO.output(4, GPIO.HIGH)
 time.sleep(3)
 GPIO.output(4, GPIO.LOW)
@@ -22,22 +25,24 @@ GPIO.output(4, GPIO.HIGH)
 time.sleep(3)
 GPIO.output(4, GPIO.LOW)
 
+# check the PIs CPU temperature
 while True:
     if cpu.temperature < 40:
         GPIO.output(4, GPIO.LOW)
-        time.sleep(300)
+        time.sleep(120)     # wait two minutes
 
     elif 40 <= cpu.temperature < 50:
         GPIO.output(4, GPIO.HIGH)
-        time.sleep(200)
+        time.sleep(240)     # wait four minutes
 
     elif 50 <= cpu.temperature < 60:
         GPIO.output(4, GPIO.HIGH)
-        time.sleep(300)
+        time.sleep(360)     # wait six minutes
 
     elif 60 <= cpu.temperature <= 80:
         GPIO.output(4, GPIO.HIGH)
-        time.sleep(600)
+        time.sleep(600)     # wait ten minutes
 
     elif cpu.temperature > 80:
         GPIO.output(4, GPIO.HIGH)
+        time.sleep(1200)    # wait 20 minutes
