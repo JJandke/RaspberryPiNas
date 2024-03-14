17bbb99515b6fbaf36483c62ba04d6ec3a67ee16
Johannes Jandke <johannesjandke@gmail.com>
2024-03-14 12:47:11 +0100

    Update stop_external_cooling.py

--- python/stop_external_cooling.py
+++ python/stop_external_cooling.py
@@ -14,39 +14,70 @@ import pigpio
 import os
 
 
-logging.basicConfig(filename="/home/config/log/external_cooling.log", level=logging.DEBUG)
-now = datetime.now()
-pwm = pigpio.pi()
-log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
+try:
+    logging.basicConfig(filename="/home/config/log/external_cooling.log", level=logging.DEBUG)
+    now = datetime.now()
+    log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
 
+except Exception as e:
+    print("FATAL ERROR! - Could not set up logging, exiting!")
+    sys.exit(-1)
 
 
 # Create log file if it does not exist.
-if not os.path.isfile("/home/config/log/external_cooling.log"):
-    logging.debug("logfile not existing... creating")
-    f = open("/home/config/log/external_cooling.log", "x")
-    f.close()
-    logging.debug("logfile created")
-else:
-    logging.debug("logfile exists")
-    pass
+try:
+    if not os.path.isfile("/home/config/log/external_cooling.log"):
+        logging.debug("{0}Logfile does not exist, therefore we will create it...".format(log_time))
+        f = open("/home/config/log/external_cooling.log", "x")
+        f.close()
+        logging.debug("{0}sucessfully created logfile".format(log_time))
+        
+    else:
+        logging.debug("{0}Logfile already exists".format(log_time))
+        pass
+
+except Exception as e:
+    logging.error("{0}".format(log_time), e)
+    logging.error("{0}Could not setup logfile, exiting!".format(log_time))
+    print("FATAL ERROR! - Could not set up logfile, exiting!")
+    sys.exit(-1)
 
 
 try:
+    logging.debug("{0}Trying to set up GPIOs".format(log_time))
+
     GPIO.setmode(GPIO.BCM)
+    logging.debug("{0}GPIO mode has been set to BCM".format(log_time))
+    
     GPIO.setwarnings(False)     # set setwarnings = False if another script uses the GPIO
+    logging.debug("{0}GPIO-Warnings have been deactivated".format(log_time))
+
     GPIO.setup(17, GPIO.OUT)    # HDD Relay
+    logging.debug("{0}GPIO #17 has been set as OUT".format(log_time))
+
     GPIO.setup(23, GPIO.OUT)    # HDD PWM
+    logging.debug("{0}GPIO #23 has been set as OUT".format(log_time))
+
+    pwm = pigpio.pi()
+    logging.debug("{0}pigpio.pi() is now pwm".format(log_time))
+
+    pwm_speed = 100
+    logging.debug("{0}pwm_speed has been set to 100".format(log_time))
     logging.debug("{0}GPIO successfully configured".format(log_time))
 
 except Exception as e:
     logging.error("{0}".format(log_time), e)
+    logging.error("{0}Could not setup GPIOs, exiting!".format(log_time))
+    sys.exit(-1)
 
 
 # It checks if the files exist. If they do, they will be deleted. If they do not exist, fade.py or strobe.py will stop if they are running.
 try:
     if os.path.exists("/home/config/code/python/.kill_cooling.txt"):
+        logging.debug("{0}Killswitch is existing. We will delete it...".format(log_time))
         os.remove("/home/config/code/python/.kill_cooling.txt")
+        logging.debug("{0}Killswitch sucessfully deleted".format(log_time))
+
 
     GPIO.output(17, 1)
     pwm.set_PWM_dutycycle(23, 0)
