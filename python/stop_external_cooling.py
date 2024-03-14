49fc232c34250b514a1f5af9d1645ec7a4705b13
Johannes Jandke <johannesjandke@gmail.com>
2024-03-14 10:48:38 +0100

    Update stop_external_cooling.py

--- python/stop_external_cooling.py
+++ python/stop_external_cooling.py
@@ -1,1 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# from https://github.com/JJandke
+#
 
+from datetime import datetime
+import logging
+import pigpio
+import os
+
+# Create log file if it does not exist.
+if not os.path.isfile("/home/config/log/external_cooling.log"):
+    f = open("/home/config/log/external_cooling.log", "x")
+    f.close()
+else:
+    pass
+
+logging.basicConfig(filename="/home/config/log/external_cooling.log", level=logging.DEBUG)
+pi = pigpio.pi()
+now = datetime.now()
+log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
+
+# It checks if the files exist. If they do, they will be deleted. If they do not exist, fade.py or strobe.py will stop if they are running.
+try:
+    if os.path.exists("/home/config/code/python/.kill_cooling.txt"):
+        os.remove("/home/config/code/python/.kill_cooling.txt")
+
+    pi.setGPIO.output(17, 0)
+    pwm.set_PWM_dutycycle(23, 0)
+    logging.info("{0}Stopped external cooling".format(log_time))
+
+except Exception as e:
+    logging.debug("{0}Could not stop external: {1}".format(log_time, e))
