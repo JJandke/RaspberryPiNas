0604611602103fd9eda5a34cea8080fa3d850c85
Johannes Jandke <johannesjandke@gmail.com>
2024-03-14 11:34:53 +0100

    Update stop_external_cooling.py

--- python/stop_external_cooling.py
+++ python/stop_external_cooling.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 # from https://github.com/JJandke
 #
+# "1" stands for "off" and "0" stands for "on", because the relay module I use does not switch when the current is present, but at 0V it does.
+# However, I only want current to flow at the relay, when the fan is running.
+# Therefore, another wiring is not possible and the problem must be solved on software side.
+# With other relay modules it might be that "0" and "1" have to be exchanged. Of course, 0 and 1 can also be replaced by "True" and "False" or "GPIO.HIGH" and "GPIO.LOW".
 
 from datetime import datetime
 import RPi.GPIO as GPIO
@@ -9,9 +13,12 @@ import logging
 import pigpio
 import os
 
-now = datetime.now()
-log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
+
 logging.basicConfig(filename="/home/config/log/external_cooling.log", level=logging.DEBUG)
+now = datetime.now()
+pwm = pigpio.py()
+log_time = now.strftime("%a-%d.%m.%Y-%H:%M:%S ")
+
 
 
 # Create log file if it does not exist.
@@ -25,14 +32,15 @@ else:
     pass
 
 
-logging.debug("start assigning variables")
-pi = pigpio.pi()
-logging.debug("assigned pi = pigpio.pi")
+try:
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)     # set setwarnings = False if another script uses the GPIO
+    GPIO.setup(17, GPIO.OUT)    # HDD Relay
+    GPIO.setup(23, GPIO.OUT)    # HDD PWM
+    logging.debug("{0}GPIO successfully configured".format(log_time))
 
-
-
-
-
+except Exception as e:
+    logging.error("{0}".format(log_time), e)
 
 
 # It checks if the files exist. If they do, they will be deleted. If they do not exist, fade.py or strobe.py will stop if they are running.
@@ -40,7 +48,7 @@ try:
     if os.path.exists("/home/config/code/python/.kill_cooling.txt"):
         os.remove("/home/config/code/python/.kill_cooling.txt")
 
-    pi.setGPIO.output(17, 0)
+    GPIO.output(17, 1)
     pwm.set_PWM_dutycycle(23, 0)
     logging.info("{0}Stopped external cooling".format(log_time))
 
